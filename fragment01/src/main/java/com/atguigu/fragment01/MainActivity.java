ee1ebc1f857ce1c02793b7016b019f2b285993b7
PeiJingXinDc <peijingxinst@gmail.com>
2016-09-27 18:23:52 +0800

    fragment1完成

--- fragment01/src/main/java/com/atguigu/fragment01/MainActivity.java
+++ fragment01/src/main/java/com/atguigu/fragment01/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.e("TAG", "onCreate...");
+        Log.e("TAG", "onCreate....");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
